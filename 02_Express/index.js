13158480cd26f5536781223a645fac8b00f7246e
abhi835 <abhishek835.hitit2020@gmail.com>
2025-01-06 18:06:50 +0530

    add .env file add some dependencies

--- 02_Express/index.js
+++ 02_Express/index.js
@@ -1,56 +1,55 @@
+import "dotenv/config";
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use(express.json())
-                           // In order to run the server first go to the target folder using cd like cd 02_Express as index.js file is inside this folder then run command like npm run dev so that the server start running and listening the port also so that the nodemon also start listening as both are inside script 
-let teaData  =[]
-let nextid  =1
+app.use(express.json());
+// In order to run the server first go to the target folder using cd like cd 02_Express as index.js file is inside this folder then run command like npm run dev so that the server start running and listening the port also so that the nodemon also start listening as both are inside script
+let teaData = [];
+let nextid = 1;
 // this is add a new tea object
-app.post('/teas',(req,res)=>{
-        
-  const{name,price} = req.body
-  const newTea = {id:nextid++,name,price}
-  teaData.push(newTea)
-  res.status(201).send(newTea)
-})
+app.post("/teas", (req, res) => {
+  const { name, price } = req.body;
+  const newTea = { id: nextid++, name, price };
+  teaData.push(newTea);
+  res.status(201).send(newTea);
+});
 //get all tea
-app.get('/teas',(req,res)=>{
-      res.status(200).send(teaData)
-      
-})
+app.get("/teas", (req, res) => {
+  res.status(200).send(teaData);
+});
 // get a tea with id
-app.get('/teas/:id',(req,res)=>{
-  const tea = teaData.find(t=>t.id===parseInt(req.params.id));
-  if(!tea){
-    return res.status(404).send('Well ,Tea not found ')
+app.get("/teas/:id", (req, res) => {
+  const tea = teaData.find((t) => t.id === parseInt(req.params.id));
+  if (!tea) {
+    return res.status(404).send("Well ,Tea not found ");
   }
   res.status(200).send(tea);
-})
+});
 
 // update the tea
-app.put('/teas/:id',(req,res)=>{
-  const tea = teaData.find(t=>t.id===parseInt(req.params.id))
-   if (!tea) {
-     return res.status(404).send("Well ,Tea not found ");
-   }
-   //now we'll update with that id
-   const {name,price} = req.body
-   tea.name  =name
-   tea.price  =price
-   res.send(200).send(tea)
-})
+app.put("/teas/:id", (req, res) => {
+  const tea = teaData.find((t) => t.id === parseInt(req.params.id));
+  if (!tea) {
+    return res.status(404).send("Well ,Tea not found ");
+  }
+  //now we'll update with that id
+  const { name, price } = req.body;
+  tea.name = name;
+  tea.price = price;
+  res.send(200).send(tea);
+});
 
-// delete the tea 
-app.delete('/teas/:id',(req,res)=> {
-   const index= teaData.findIndex(t=>t.id===parseInt(req.params.id))
-   if(index=== -1){
-         return res.status(404).send('tea not found')
-   }
-   teaData.splice(index,1)
-   return res.status(204).send("tea deleted successfully");
-})
+// delete the tea
+app.delete("/teas/:id", (req, res) => {
+  const index = teaData.findIndex((t) => t.id === parseInt(req.params.id));
+  if (index === -1) {
+    return res.status(404).send("tea not found");
+  }
+  teaData.splice(index, 1);
+  return res.status(204).send("tea deleted successfully");
+});
 
 // app.get("/", (req, res) => {
 
@@ -62,9 +61,6 @@ app.delete('/teas/:id',(req,res)=> {
 // app.get("/twitter", (req, res) => {
 //   res.send("ice tea is on twitter now");
 // });
-
-
-
 
 app.listen(port, () => {
   console.log(`Server is running at port: ${port}....`);
